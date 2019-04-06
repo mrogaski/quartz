b08d258a159e50eb0ed6c59adb19dcbcc7783e39
Mark Rogaski <mrogaski@pobox.com>
2019-04-06 10:54:32 -0400

    Added clock states. (#5)

--- core/clock.go
+++ core/clock.go
@@ -6,14 +6,24 @@ import (
 )
 
 const (
-	PeriodDuration      = time.Minute * 30	// Default duration for a bout period.
-	LineupDuration      = time.Second * 30	// Default time between jams.
-	JamDuration         = time.Minute * 2	// Default maximum jam time.
-	TeamTimeoutDuration = time.Minute * 1	// Default duration of a team timeout.
+	PeriodDuration      = time.Minute * 30 // Default duration for a bout period.
+	LineupDuration      = time.Second * 30 // Default time between jams.
+	JamDuration         = time.Minute * 2  // Default maximum jam time.
+	TeamTimeoutDuration = time.Minute * 1  // Default duration of a team timeout.
+)
+
+type PeriodClockState string
+
+// PeriodClockState reflects the running state of the period clock.
+const (
+	JamPending    PeriodClockState = "pending" // JamPending indicates the lineup period before the start of a jam.
+	JamActive     PeriodClockState = "active"  // JamActive indicates and active jam.
+	PeriodTimeout PeriodClockState = "timeout" // PeriodTimeout indicates a timeout is in effect.
 )
 
 // PeriodClock contains the elapsed time for the current period, lineup, jam, and timeout.
 type PeriodClock struct {
+	State          PeriodClockState
 	PeriodElapsed  time.Duration
 	LineupElapsed  time.Duration
 	JamElapsed     time.Duration
